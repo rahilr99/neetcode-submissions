c903b44f2492ad2e7940fb2013f81a756d3ac867
Rahil R <141838511+rahilr99@users.noreply.github.com>
2026-08-12 10:21:48 -0400

    Add: python-list-comprehension - submission-1

---
+++ Python Coding Interviews/python-list-comprehension/submission-1.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def create_list_of_odds(n: int) -> List[int]:
+    oddList = [i for i in range(1,n+1,2)]
+    return oddList
+
+
+# do not modify below this line
+print(create_list_of_odds(1))
+print(create_list_of_odds(5))
+print(create_list_of_odds(6))
+print(create_list_of_odds(10))
